0a9e2b06b90e0e11703a2efb98fc22be6d795b74
Steve Greenberg <sgreenberg@rscale.io>
2023-06-22 08:12:25 -0600

    initial

---
+++ src/main/java/io/skylounge/hellogradlew/HelloGradlewApplication.java
@@ -0,0 +1,13 @@
+package io.skylounge.hellogradlew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGradlewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGradlewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/skylounge/hellogradlew/HelloGradlewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.skylounge.hellogradlew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGradlewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
